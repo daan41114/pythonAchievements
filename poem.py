01b319f11c129b6ee3b98f9a4253c0a8042bc5fa
daan41114 <rendomgamer41114@gmail.com>
2020-09-23 12:39:53 +0200

    .

---
+++ poem.py
@@ -0,0 +1,18 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> vers1 = 'mijn naam is daan'
+>>> vers2 = 'ik woon in heerhugowaart, want dat is het waard'
+>>> vers4 = 'ik haat rennen, zelfs al kan ik dan dingen verkennen'
+>>> vers5 = 'mijn favoriete spel is smash bros, ja dat is het spel dat ik koos'
+>>> vers5 = 'mijn favoriete spel is smash bros, ja dat is het spel dat ik koos'
+>>> vers6 = 'verder kijk ik veel anime, en nee niet op de wc'
+>>> vers7 = 'dit is het einde, fijn he.'
+>>> print(vers1,vers2,vers3,vers4,vers5,vers6,vers7)
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    print(vers1,vers2,vers3,vers4,vers5,vers6,vers7)
+NameError: name 'vers3' is not defined
+>>> vers3 = 'mijn hobby is gamen, dat is alles dat ik ken'
+>>> print(vers1,vers2,vers3,vers4,vers5,vers6,vers7)
+mijn naam is daan ik woon in heerhugowaart, want dat is het waard mijn hobby is gamen, dat is alles dat ik ken ik haat rennen, zelfs al kan ik dan dingen verkennen mijn favoriete spel is smash bros, ja dat is het spel dat ik koos verder kijk ik veel anime, en nee niet op de wc dit is het einde, fijn he.
+>>> 
