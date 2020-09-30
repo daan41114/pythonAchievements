59737f224ed82591526ac10ad2913d34c521f2b7
daan41114 <rendomgamer41114@gmail.com>
2020-09-30 11:34:19 +0200

    character opdracht toegevoegt

---
+++ character opdracht.py
@@ -0,0 +1,26 @@
+naam = 'daan'
+level = 'lv 50'
+headgear = 'headgear   knight helmed'
+bodygear = 'bodygear   caveman outfit'
+boots = 'boots   strong boots'
+special_ability = 'special_ability   dash attack'
+headgear_level = 'headgear   lv 20'
+bodygear_level = 'bodygear   lv 20'
+boots_level = 'boots   lv 25'
+weapon = 'weapon   master axe'
+bow = 'bow   valkarie bow'
+weapon_durability = 'weapon   100/1000'
+bow_durability = 'bow   300/500'
+print (naam)
+print (level)
+print (special_ability)
+print (headgear)
+print (headgear_level)
+print (bodygear)
+print (bodygear_level)
+print (boots)
+print (boots_level)
+print (weapon)
+print (weapon_durability)
+print (bow)
+print (bow_durability)
