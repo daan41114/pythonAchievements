68a3dea1a4c6aed8daf8a46232550a120878b200
daan41114 <rendomgamer41114@gmail.com>
2020-10-07 12:47:47 +0200

    this time i really fixed the bug

--- PYTB1L3DailyChoices.py
+++ PYTB1L3DailyChoices.py
@@ -10,13 +10,13 @@ print('D. I freak out becouse I am normaly not up at 12:00, and go to sleep as f
 print('or own answer')
 inputvariable = input()
 
-if input() =='A':
+if inputvariable =='A':
     print('cool you know when to stop,(unlike me)')
-elif input() =='B':
+elif inputvariable =='B':
     print('wow you really have a bad sleeping schedule')
-elif input() =='C':
+elif inputvariable =='C':
     print('you are me')
-elif input() =='D':
+elif inputvariable =='D':
     print('you messed up')
 else:
     print('haha I i can not read')
@@ -28,11 +28,11 @@ print('C. I start making a scedule for when I make it')
 print('or own anwser')
 inputvariable =input()
 
-if input() =='A':
+if inputvariable =='A':
     print('you think howework first then gaming')
-elif input() =='B':
+elif inputvariable =='B':
     print('you are mr incredible')
-elif input() =='c':
+elif inputvariable =='c':
     print('you are a cotrol freak and later realise you did not give yourself enough time')
 else:
     print('haha I stil can not read')
@@ -45,14 +45,14 @@ print('D. I try talking myself out of trouble')
 print('or own anwser')
 inputvariable = input()
 
-if input() =='A':
+if inputvariable =='A':
     print('you pay alot of attention in class even if you do not need to.')
-elif input() =='B':
+elif inputvariable =='B':
     print('you just say that but in reality you still go in class.')
-elif input() =='C':
+elif inputvariable =='C':
     print('you always make up the stupidest excuses.')
     print('or you got an entire piece of paper of fake excuses')
-elif input() =='D':
+elif inputvariable =='D':
     print('you always try talking your way out of things and it almost never works')
 else:
     print('you know the gist by now')
@@ -64,11 +64,11 @@ print('C. I do not eat breakfast')
 print('or own anwser')
 inputVariabele = input()
 
-if input() =='A':
+if inputvariable =='A':
     print('you are almost everyone')
-elif input() =='B':
+elif inputvariable =='B':
     print('you are like me')
-elif input() =='c':
+elif inputvariable =='c':
     print('why not?')
 else:
     print('why are you still doing own anwser')
@@ -79,11 +79,11 @@ print('b. smash bros ultimate')
 print('C. smash bros ultimate')
 inputvariable = input()
 
-if input() =='A':
+if inputvariable =='A':
     print('you are right.')
-elif input() =='B':
+elif inputvariable =='B':
     print('correct!!!')
-elif input() =='C':
+elif inputvariable =='C':
     print('indeed.')
 else:
     print('I do not know what you awnserd but you are wrong!!!')
