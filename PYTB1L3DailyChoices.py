46407565dcb409147360e55ef1c762c21b3157e8
daan41114 <rendomgamer41114@gmail.com>
2020-10-07 12:38:15 +0200

    added questions

--- PYTB1L3DailyChoices.py
+++ PYTB1L3DailyChoices.py
@@ -3,10 +3,10 @@ print('please anwser in caps if choosing one of the anwsers')
 print('also you might habe to anwser multible times do not know why')
 print ('-------------------------------------')
 print('you are up at 12:00Am, gaming what do you do')
-print('A I go to sleep its 12:00Am.')
-print('B I keep gaming its only 12:00Am.')
-print('C I go to bed and watch youtube or something.')
-print('D I freak out becouse I am normaly not up at 12:00, and go to sleep as fast as possible resulting in not being able to sleep')
+print('A. I go to sleep its 12:00Am.')
+print('B. I keep gaming its only 12:00Am.')
+print('C. I go to bed and watch youtube or something.')
+print('D. I freak out becouse I am normaly not up at 12:00, and go to sleep as fast as possible resulting in not being able to sleep')
 print('or own answer')
 input()
 
@@ -22,10 +22,11 @@ else:
     print('haha I i can not read')
 print ('-------------------------------------')
 print('you have homework to do at 10:00Pm, what do you do.')
-print('A I make it right away.')
-print('B I start gaming I got time.')
-print('C I start making a scedule for when I make it')
+print('A. I make it right away.')
+print('B. I start gaming I got time.')
+print('C. I start making a scedule for when I make it')
 print('or own anwser')
+input()
 
 if input() =='A':
     print('you think howework first then gaming')
@@ -35,6 +36,57 @@ elif input() =='c':
     print('you are a cotrol freak and later realise you did not give yourself enough time')
 else:
     print('haha I stil can not read')
+print ('-------------------------------------')
+print('you are at school 5 minutes to late officialy absent, what do you do.')
+print('A. I walk in class and stil attend in class')
+print('B. I do not go in class im already absent so it whould not matter')
+print('C. I go in class and make up an excuse for why I am late')
+print('D. I try talking myself out of trouble')
+print('or own anwser')
+input()
+
+if input() =='A':
+    print('you pay alot of attention in class even if you do not need to.')
+elif input() =='B':
+    print('you just say that but in reality you still go in class.')
+elif input() =='C':
+    print('you always make up the stupidest excuses.')
+    print('or you got an entire piece of paper of fake excuses')
+elif input() =='D':
+    print('you always try talking your way out of things and it almost never works')
+else:
+    print('you know the gist by now')
+print ('-------------------------------------')
+print('you get up what is your breakfast.')
+print('A. I eat some bread')
+print('b. I eat sereal.')
+print('C. I do not eat breakfast')
+print('or own anwser')
+input()
+
+if input() =='A':
+    print('you are almost everyone')
+elif input() =='B':
+    print('you are like me')
+elif input() =='c':
+    print('why not?')
+else:
+    print('why are you still doing own anwser')
+print ('-------------------------------------')
+print('what is the best game')
+print('A. smash bros ultimate')
+print('b. smash bros ultimate')
+print('C. smash bros ultimate')
+input()
+
+if input() =='A':
+    print('you are right.')
+elif input() =='B':
+    print('correct!!!')
+elif input() =='C':
+    print('indeed.')
+else:
+    print('I do not know what you awnserd but you are wrong!!!')
 
 print('that was all')
 print('thank you for anwsering my questions')
