f870d74fe93bef5986d1ebfaf779aaf6d6017d77
daan41114 <rendomgamer41114@gmail.com>
2020-10-07 12:06:20 +0200

    added PYTB1L3DailyChoices.py

---
+++ PYTB1L3DailyChoices.py
@@ -0,0 +1,38 @@
+print('here are some questions')
+print('please anwser in caps if choosing one of the anwsers')
+print('also you might habe to anwser multible times do not know why')
+print ('-------------------------------------')
+print('you are up at 12:00Am, gaming what do you do')
+print('A I go to sleep its 12:00Am.')
+print('B I keep gaming its only 12:00Am.')
+print('C I go to bed and watch youtube or something.')
+print('D I freak out becouse I am normaly not up at 12:00, and go to sleep as fast as possible resulting in not being able to sleep')
+print('or own answer')
+input()
+
+if input() =='A':
+    print('cool you know when to stop,(unlike me)')
+elif input() =='B':
+    print('wow you really have a bad sleeping schedule')
+elif input() =='C':
+    print('you are me')
+elif input() =='D':
+    print('you messed up')
+else:
+    print('haha I i can not read')
+print ('-------------------------------------')
+print('you have homework to do at 10:00Pm, what do you do.')
+print('A I make it right away.')
+print('B I start gaming I got time.')
+print('C I start making a scedule for when I make it')
+print('or own anwser')
+
+if input() =='A':
+    print('you think howework first then gaming')
+elif input() =='B':
+    print('you are mr incredible')
+elif input() =='c':
+    print('you are a cotrol freak and later realise you did not give yourself enough time')
+else:
+    print('haha I stil can not read')
+
