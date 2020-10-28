9f1a15f927edcf40bded4b66fd8104b8b2f84699
daan41114 <rendomgamer41114@gmail.com>
2020-10-28 12:03:01 +0100

    added final countdown

---
+++ PYTB1L4TheFinalCountdown.py
@@ -0,0 +1,10 @@
+import time
+getallen = 11
+for x in range(1):
+    while getallen >= 1:
+        time.sleep(1)
+        getallen-= 1
+        print(getallen)
+while getallen == 0:
+    print('get stickbugd lol')
+    break
